2cea93d3d5bc5a9349395d95206e1d6de553e2db
Ansuman Behera <sonuansuman833@gmail.com>
2021-04-20 02:53:45 +0530

    its me

---
+++ index.js
@@ -0,0 +1,7 @@
+function convertbcd() {
+    var fromCode = document.getElementById("select").value;
+    let result = document.getElementById("result");
+    if (fromCode === "Silver"){
+    result.innerHTML = "wow";
+    }
+}
